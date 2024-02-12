95e8ebe69a35517d54edd72a37995e73f5dd4f6f
Jarno N. Alanko <alanko.jarno@gmail.com>
2024-02-12 16:17:25 +0200

    Update

--- README.md
+++
@@ -1,1 +0,0 @@
-This thing takes a fasta file of reference sequences and a fasta file of patterns, and prints how many distinct patterns there are and the sum of frequencies of those patterns.
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use bio::data_structures::suffix_array::suffix_array;
 use bio::data_structures::fmindex::BackwardSearchResult;
 use clap::{Command, Arg};
 use jseqio::reader::SeqRecordProducer;
+use rand::seq::index::sample;
+use rand::{self, Rng, RngCore, SeedableRng};
+use rand::rngs::StdRng;
 
 fn count(index: &FMIndex<&Vec<u8>, &Vec<usize>, &Occ>, pattern: &[u8]) -> usize {
     let res = index.backward_search(pattern.iter());
@@ -19,52 +22,86 @@ fn count(index: &FMIndex<&Vec<u8>, &Vec<usize>, &Occ>, pattern: &[u8]) -> usize 
     } else { 0 }
 } 
 
-fn main() {
-    let cli = Command::new("total-string-frequency")
-        .arg_required_else_help(true)
-        .arg(Arg::new("unitigs")
-            .help("Fasta format")
-            .short('u')
-            .value_parser(clap::value_parser!(std::path::PathBuf)))
-        .arg(Arg::new("patterns")
-            .help("Fasta format")
-            .short('p')
-            .value_parser(clap::value_parser!(std::path::PathBuf)));
+fn generate_random_dna_string(length: usize, seed: u64) -> Vec<u8> {
+    let mut rng = StdRng::seed_from_u64(seed);
+    let alphabet: Vec<u8> = vec![b'A', b'C', b'G', b'T'];
 
-    let matches = cli.get_matches();
-    let unitigs_file: &std::path::PathBuf = matches.get_one("unitigs").unwrap();
-    let patterns_file: &std::path::PathBuf = matches.get_one("patterns").unwrap();
-
-    // Read unitigs and concatenate them with dollar separators
-    let mut unitigs_concat = Vec::<u8>::new();
-    let mut unitig_reader = jseqio::reader::DynamicFastXReader::from_file(unitigs_file).unwrap();
-    while let Some(rec) = unitig_reader.read_next().unwrap(){
-        unitigs_concat.extend(rec.seq);
-        unitigs_concat.push(b'$');
+    let mut s = Vec::<u8>::new();
+    for _ in 0..length{
+        s.push(alphabet[(rng.next_u64() % 4) as usize]);
     }
+    s
+    
+}
+
+fn main() {
+
+    let n = 1000_000_usize;
+    let seed = 1234;
+    let mut data = generate_random_dna_string(n, seed);
+
+    data.push(b'$'); // The BWT library needs this?
 
     // Build the FM index
+    eprintln!("Building FM index");
     let alphabet = dna::n_alphabet();
-    let sa = suffix_array(unitigs_concat.as_slice());
-    let bwt = bwt(unitigs_concat.as_slice(), &sa);
+    let sa = suffix_array(data.as_slice());
+    let bwt = bwt(data.as_slice(), &sa);
     let less = less(&bwt, &alphabet);
     let occ = Occ::new(&bwt, 3, &alphabet);
     let index: FMIndex<&Vec<u8>, &Vec<usize>, &Occ> = FMIndex::new(&bwt, &less, &occ);
+    eprintln!("FM index built");
 
-    // Count occurrences of distinct patterns
-    let mut searched_patterns = std::collections::HashSet::<Vec<u8>>::new();
-    let mut pattern_reader = jseqio::reader::DynamicFastXReader::from_file(patterns_file).unwrap();
-    let mut total_count = 0_usize;
-    while let(Some(rec)) = pattern_reader.read_next().unwrap(){
-        let pattern = rec.seq;
-        if searched_patterns.contains(pattern){
-            continue;
-        } else{
-            searched_patterns.insert(pattern.to_owned());
-            total_count += count(&index, pattern);
+    let mut sampled_endpoints = Vec::<usize>::new();
+    let mut lengths = Vec::<usize>::new();
+    let k = 63;
+    for i in 0..n-k+1{
+        let kmer = &data[i..i+k];
+        let mut finimizer: Option<&[u8]> = None;
+        let mut f_start = 0;
+        let mut f_end = kmer.len();
+        for start in 0..kmer.len(){
+            for end in start+1..kmer.len(){
+                let x = &kmer[start..end];
+                let freq = count(&index, x);
+                if freq == 1 {
+                    match finimizer{
+                        None => {
+                            finimizer = Some(x);
+                            f_start = i + start;
+                            f_end = i + end;
+                        },
+                        Some(cur) => {
+                            if x.len() < cur.len() || (x.len() == cur.len() && x < cur){
+                                finimizer = Some(x);
+                                f_start = i + start;
+                                f_end = i + end;
+                            }
+                        }
+                    }
+                    break; // No reason to check further endpoints because this one was unique already
+                }
+            }
         }
+        match sampled_endpoints.last(){
+            Some(prev) => {
+                if *prev != f_end{
+                    sampled_endpoints.push(f_end);
+                    lengths.push(f_end - f_start);
+                }
+            },
+            None => {
+                sampled_endpoints.push(f_end);
+                lengths.push(f_end - f_start);
+            }
+        }
+
+        println!("{}, {}, {}", f_end, String::from_utf8(kmer.to_vec()).unwrap(), String::from_utf8(finimizer.unwrap().to_vec()).unwrap());
     }
-    println!("Distinct patterns: {}", searched_patterns.len());
-    println!("Total occurrences: {}", total_count);
+
+    assert_eq!(sampled_endpoints.len(), lengths.len());
+    println!("{}/{}", sampled_endpoints.len(), n);
+    println!("Mean length: {}", lengths.iter().sum::<usize>() as f64 / lengths.len() as f64);
+
 
 }
